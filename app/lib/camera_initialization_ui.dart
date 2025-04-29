161e62fd7fadf56a4933603bd0ca8b3f1f99eef7
kos6490 <kos-6490@naver.com>
2025-04-29 22:35:23 +0900

    modify: import for Future use, asynchronous import list of available cameras ready for use in UI

--- app/lib/camera_initialization_ui.dart
+++ app/lib/camera_initialization_ui.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'dart:async';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key});
 
   @override
   State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<List<CameraDescription>>? _camerasFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _camerasFuture = _initializeCameras();
+  }
 }
