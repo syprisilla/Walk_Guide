5b6f918ee90b3f9aa31f65661fd5d632626f0de2
kos6490 <kos-6490@naver.com>
2025-04-29 22:30:19 +0900

    modify: add pacakages to use basic UI components and camera features, define root widgets

---
+++ app/lib/camera_initialization_ui.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
