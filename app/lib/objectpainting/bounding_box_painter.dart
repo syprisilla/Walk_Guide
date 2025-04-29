6da67a59398e4c37a8f9f251df7cbf84af97d2c6
xnoa03 <xnoa03@naver.com>
2025-04-30 00:13:20 +0900

    add:class_BoundinBoxUtils

--- app/lib/objectpainting/bounding_box_painter.dart
+++ app/lib/objectpainting/bounding_box_painter.dart
@@ -2,3 +2,12 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_commons/google_mlkit_commons.dart'; 
 import 'package:camera/camera.dart'; 
+
+class BoundingBoxUtils {
+  static Rect scaleAndTranslateRect({
+    required Rect boundingBox,
+    required Size imageSize, 
+    required Size canvasSize,
+    required InputImageRotation rotation, 
+    required CameraLensDirection cameraLensDirection, 
+  })
