543657af6ae095ca3c99aa03668447cbc4d6a89c
syprisilla <kt28805546@gmail.com>
2025-04-29 22:44:09 +0900

    feat: Update Navigation button

--- app/lib/main_screen.dart
+++ app/lib/main_screen.dart
@@ -3,26 +3,34 @@ import 'package:walk_guide/step_counter_page.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('메인 화면'),
-      ),
+      appBar: AppBar(title: const Text('메인 화면')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '안녕하세요!',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
+            const Text('즐거운 하루 되세요!', style: TextStyle(fontSize: 24)),
+            const SizedBox(height: 20),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 20,
+                ),
+                textStyle: const TextStyle(fontSize: 20),
+              ),
               onPressed: () {
-                MaterialPageRoute(builder: (context) => const StepCounterPage());
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const StepCounterPage(),
+                  ),
+                );
               },
-              child: Text('버튼'),
+              child: const Text('보행 시작하기'),
             ),
           ],
         ),
