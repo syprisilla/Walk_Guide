8548ec10cf8017e28df874f976c69bb1648b0412
kos6490 <kos-6490@naver.com>
2025-04-30 17:53:56 +0900

    modify: add pacakages to use object detection

---
+++ app/lib/camera_screen.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'dart:isolate';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_commons/google_mlkit_commons.dart';
+import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
+import 'dart:io';
