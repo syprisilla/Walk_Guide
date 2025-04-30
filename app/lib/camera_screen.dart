cd51132e0a53508b2479e21c649c13d18de8477d
kos6490 <kos-6490@naver.com>
2025-04-30 17:59:53 +0900

    modify: add screen widget class definitions for real-time object detection

--- app/lib/camera_screen.dart
+++ app/lib/camera_screen.dart
@@ -7,3 +7,13 @@ import 'package:flutter/services.dart';
 import 'package:google_mlkit_commons/google_mlkit_commons.dart';
 import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
 import 'dart:io';
+
+class RealtimeObjectDetectionScreen extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const RealtimeObjectDetectionScreen({Key? key, required this.cameras})
+    : super(key: key);
+
+  @override
+  _RealtimeObjectDetectionScreenState createState() =>
+      _RealtimeObjectDetectionScreenState();
+}
