cf9bcc819a43d66b4f3911862d8e6582bafe1dd5
xnoa03 <xnoa03@naver.com>
2025-05-02 01:59:31 +0900

    add:funtion_paintNameTag

--- app/lib/objectpainting/name_tag_painter.dart
+++ app/lib/objectpainting/name_tag_painter.dart
@@ -2,3 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart'; // Label
 
 class NameTagUtils {
+  static void paintNameTag({
+    required Canvas canvas,
+    required Label label,
+     required Rect boundingBoxRect, // 기준 박스 위치
+    required Size canvasSize,
+  }) {
+    final TextPainter textPainter = TextPainter(
+      text: TextSpan(
+        text: ' ${label.text} (${(label.confidence * 100).toStringAsFixed(0)}%) ',
