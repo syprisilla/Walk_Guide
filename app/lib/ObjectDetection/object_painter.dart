2254f8a1956edbdf2f6af649718d252b4183bc4f
xnoa03 <xnoa03@naver.com>
2025-05-03 15:57:50 +0900

    add:class_ObjectPainter

--- app/lib/ObjectDetection/object_painter.dart
+++ app/lib/ObjectDetection/object_painter.dart
@@ -4,3 +4,7 @@ import 'package:camera/camera.dart';
 import 'bounding_box_painter.dart'; // 바운딩 박스 유틸리티 임포트
 import 'name_tag_painter.dart';   // 네임태그 유틸리티 임포트
 import 'package:google_mlkit_commons/google_mlkit_commons.dart'; // InputImageRotation
+
+class ObjectPainter extends CustomPainter {
+  final List<DetectedObject> objects;
+  final Size imageSize;
