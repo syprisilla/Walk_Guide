c85d048f63364637959b6bbe7df2a68ed1def173
xnoa03 <xnoa03@naver.com>
2025-05-03 15:56:43 +0900

    add:imports

--- app/lib/ObjectDetection/object_painter.dart
+++ app/lib/ObjectDetection/object_painter.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
 import 'package:camera/camera.dart';
+import 'bounding_box_painter.dart'; // 바운딩 박스 유틸리티 임포트
+import 'name_tag_painter.dart';   // 네임태그 유틸리티 임포트
+import 'package:google_mlkit_commons/google_mlkit_commons.dart'; // InputImageRotation
