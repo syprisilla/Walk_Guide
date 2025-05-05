d7b334e5c5110acaa95b151c9634a8a33fbff2d7
Jeonsooyoung <jsooyoung05@gmail.com>
2025-05-05 12:15:02 +0900

    add: initialRoute

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:walk_guide/walk_session.dart';
 import 'package:walk_guide/splash_screen.dart';
+import 'package:walk_guide/login_screen.dart';      
+import 'package:walk_guide/main_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
@@ -25,7 +27,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Flutter App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const SplashScreen(),
+      initialRoute: '/',  
+      routes: {
+        '/': (context) => const SplashScreen(),       // 초기 화면
+        '/login': (context) => const LoginScreen(),   // 로그인 화면
+        '/home': (context) => const MainScreen(),     // 로그인 성공 시 이동할 홈 화면
+      },
     );
   }
 }
