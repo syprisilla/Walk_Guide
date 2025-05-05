ff5e78e803c78fd19724dd179d32df35c0535bce
kos6490 <kos-6490@naver.com>
2025-05-05 15:10:42 +0900

    add: add import statements required for ml kit object detection

---
+++ app/lib/ObjectDetection/mlkit_object_detection.dart
@@ -0,0 +1,6 @@
+import 'dart:isolate';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_commons/google_mlkit_commons.dart';
+import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
+import 'dart:io';
