f5a069f208bd543c60f318bbee7444bcc7cac425
kos6490 <kos-6490@naver.com>
2025-05-05 16:51:22 +0900

    modify: add function to convert integer rotation angle to inputimagerotation enumeration of ml kit

--- app/lib/ObjectDetection/mlkit_object_detection.dart
+++ app/lib/ObjectDetection/mlkit_object_detection.dart
@@ -153,3 +153,18 @@ InputImageRotation? _getImageRotationImpl(
     return _rotationIntToInputImageRotation(compensatedRotation);
   }
 }
+
+InputImageRotation _rotationIntToInputImageRotation(int rotation) {
+  switch (rotation) {
+    case 0:
+      return InputImageRotation.rotation0deg;
+    case 90:
+      return InputImageRotation.rotation90deg;
+    case 180:
+      return InputImageRotation.rotation180deg;
+    case 270:
+      return InputImageRotation.rotation270deg;
+    default:
+      return InputImageRotation.rotation0deg;
+  }
+}
