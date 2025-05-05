7698773d1457b98b4aaa5ea4b80cb30630fbc244
kos6490 <kos-6490@naver.com>
2025-05-05 15:15:18 +0900

    modify: add ml kit object detector initialization function

--- app/lib/ObjectDetection/mlkit_object_detection.dart
+++ app/lib/ObjectDetection/mlkit_object_detection.dart
@@ -4,3 +4,13 @@ import 'package:flutter/services.dart';
 import 'package:google_mlkit_commons/google_mlkit_commons.dart';
 import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
 import 'dart:io';
+
+ObjectDetector initializeObjectDetector() {
+  print("Initializing ML Kit detector...");
+  final options = ObjectDetectorOptions(
+    mode: DetectionMode.stream,
+    classifyObjects: true,
+    multipleObjects: true,
+  );
+  return ObjectDetector(options: options);
+}
