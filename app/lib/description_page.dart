f04668ad2e1d01c4c49aba715d294590cbe3cd5f
syprisilla <kt28805546@gmail.com>
2025-05-06 23:36:45 +0900

    feat: fix DescriptionPage

--- app/lib/description_page.dart
+++ app/lib/description_page.dart
@@ -1,54 +1,45 @@
 import 'package:flutter/material.dart';
 
 class DescriptionPage extends StatelessWidget {
-  const DescriptionPage({super.key});
+  final List<String> titles = [
+    '계정 정보',
+    '개인정보처리방침',
+    '이용 약관',
+    '사업자 정보',
+    '오픈 소스 라이브러리',
+    '앱 업데이트',
+  ];
+
+  final List<VoidCallback> actions = [
+    () {}, // 계정 정보
+    () {}, // 개인정보처리방침
+    () {}, // 이용 약관
+    () {}, // 사업자 정보
+    () {}, // 오픈 소스
+    () {}, // 앱 업데이트
+  ];
+
+  DescriptionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300], 
       appBar: AppBar(
-        title: const Text('무슨 기능이 있나요?'),
-        backgroundColor: Colors.amber,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-
-            const Text(
-              '• 지도 •',
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const Text('→ 사용자의 현재 위치와 주변 정보가 나타나요!',
-            style: TextStyle(fontSize: 20),
-            ),
-
-            const SizedBox(height: 20),
-
-            const Text(
-              '• 객체 감지 •',
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const Text('→ 사용자 주변 객체를 탐지해요. 객체의 크기를 알고 정의할 수 있어요.',
-            style: TextStyle(fontSize: 20),
-            ),
-
-            const SizedBox(height: 20),
-
-            const Text(
-              '• 음성 안내 •',
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const Text('→ 사용자의 보행 속도를 ai가 파악하고 안내해요. 음성 피드백을 제공해요.',
-            style: TextStyle(fontSize: 20),
-            ),
-          ],
+        title: Text('소개', style: TextStyle(fontWeight: FontWeight.bold)),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
         ),
+      ),
+      body: ListView.builder(
+        itemCount: titles.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(titles[index]),
+            trailing: Icon(Icons.chevron_right),
+            onTap: actions[index],
+          );
+        },
       ),
     );
   }
--- app/lib/main_screen.dart
+++ app/lib/main_screen.dart
@@ -29,7 +29,7 @@ class _MainScreenState extends State<MainScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const DescriptionPage(),
+                    builder: (context) => DescriptionPage(),
                   ),
                 );
               },
