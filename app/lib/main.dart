4e78482a018f4714cb19183a14ee1ccabe27a772
syprisilla <kt28805546@gmail.com>
2025-05-19 09:29:41 +0900

    add: datetime_adapter.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,13 +3,17 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:walk_guide/walk_session.dart';
 import 'package:walk_guide/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:walk_guide/datetime_adapter.dart'; //  DateTimeAdapter import
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
 
-  Hive.registerAdapter(WalkSessionAdapter()); // 어댑터 등록
-  await Hive.openBox<WalkSession>('walk_sessions');
+  Hive.registerAdapter(WalkSessionAdapter()); // 기존 어댑터 등록
+  Hive.registerAdapter(DateTimeAdapter()); //  DateTime 어댑터 등록
+
+  await Hive.openBox<WalkSession>('walk_sessions'); // 기존 세션 박스
+  await Hive.openBox<DateTime>('recent_steps'); // 실시간 속도용 박스
 
   await Firebase.initializeApp();
 
